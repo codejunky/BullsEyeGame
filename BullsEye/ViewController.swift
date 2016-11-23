10dbe5207192af70a23b39adf6e0ad5c189615cc
Oussama Bouguerne <oussama.bouguerne@metaworks.xyz>
2016-11-23 05:52:27 +0100

    Add functionality to get the position of the slider

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,26 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+  var currentValue: Int = 50
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  @IBAction func showAlert() {
+    let message = "The value of the slider is: \(currentValue)"
+    let alert = UIAlertController(title: "Hello, World",
+                                  message: message,
+                                  preferredStyle: .alert)
+    
+    let action = UIAlertAction(title: "Awesome", style: .default,
+                               handler: nil)
+    alert.addAction(action)
+    
+    present(alert, animated: true, completion: nil)
+  }
+  
+  @IBAction func sliderMoved(_ slider: UISlider) {
+    currentValue = lroundf(slider.value)
+  }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
-    @IBAction func showAlert() {
-        let alert = UIAlertController(title: "Hello, World",
-                                      message: "This is my first app!",
-                                      preferredStyle: .alert)
-        
-        let action = UIAlertAction(title: "Awesome", style: .default,
-                                   handler: nil)
-        alert.addAction(action)
-        
-        present(alert, animated: true, completion: nil)
-    }
- }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
